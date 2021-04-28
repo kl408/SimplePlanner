7441de27e4d2119c5182df06fcf298d871d00182
Michelle Cheng <michellecheng.2017@gmail.com>
2021-04-28 17:46:11 -0400

    Initial Commit

---
+++ SimplePlanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimplePlanner
+//
+//  Created by Michelle Cheng on 4/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
