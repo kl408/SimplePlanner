d8f3b64f40cdb722599f59a4e3b1286a292d3bb0
Michelle Cheng <michellecheng.2017@gmail.com>
2021-05-15 00:15:38 -0400

    implemented date picker

--- SimplePlanner/TasksViewController.swift
+++ SimplePlanner/TasksViewController.swift
@@ -23,20 +23,46 @@ class TasksViewController: UIViewController {
         task["dueDate"] = dateField.text!
         task["owner"] = PFUser.current()!
         
-        task.saveInBackground { (success, error) in
-            if success {
-                self.dismiss(animated: true, completion: nil)
-                print("saved!")
-            } else{
-                print("error!")
+        if let text = taskField.text, text.isEmpty{
+            self.dismiss(animated: true, completion: nil)
+        } else {
+            task.saveInBackground { (success, error) in
+                if success {
+                    self.dismiss(animated: true, completion: nil)
+                    print("saved!")
+                } else{
+                    print("error!")
+                }
             }
         }
     }
+    
+    let datePicker = UIDatePicker()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        createDatePicker()
     }
     
+    func createDatePicker() {
+        let toolbar = UIToolbar();
+        toolbar.sizeToFit()
+        let doneBtn = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePressed))
+        toolbar.setItems([doneBtn], animated: true)
+        dateField.inputAccessoryView = toolbar
+        datePicker.preferredDatePickerStyle = .wheels
+        dateField.inputView = datePicker
+        datePicker.datePickerMode = .date
+    }
+    
+    @objc func donePressed() {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        formatter.dateFormat = "EEEE, MMM d, yyyy"
+        dateField.text = formatter.string(from: datePicker.date)
+        self.view.endEditing(true)
+    }
 
     /*
     // MARK: - Navigation
